0670ed4903f7d2b362899c4c9791c806f5512eec
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-09-02 23:13:52 +0530

    Create overall css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Profile from "./components/profile/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
